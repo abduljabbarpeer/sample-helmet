6747559c108c061f47efe6773a0cf21a2ceeb947
abdul <abduljabbarpeer@gmail.com>
2021-06-02 01:27:11 +0530

    comm1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import { Helmet } from 'react-helmet';
+import { useState } from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import Page1 from './Components/Page1';
 import Page2 from './Components/Page2';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className='App'>
       <h1>MAIN PAGE WITH DIFFERENT PAGES LINK</h1>
-      <iframe
+      {/* <iframe
         width='560'
         height='315'
         src='https://www.youtube.com/embed/-QjKbULq7CE'
@@ -15,7 +16,8 @@ function App() {
         frameborder='0'
         allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
         allowfullscreen
-      ></iframe>
+      ></iframe> */}
+      <button onClick={() => setCount(count + 1)}>{count}</button>
       <Link to='/first'>FIRST PAGE</Link>
       <Link to='/second'>SECOND PAGE</Link>
       <Switch>
