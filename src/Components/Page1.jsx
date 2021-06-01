792f9bbb9fdc4a99bde4f22ba7b03fa16ee3b337
abdul <abduljabbarpeer@gmail.com>
2021-06-01 17:44:21 +0530

    second commit

--- src/Components/Page1.jsx
+++ src/Components/Page1.jsx
@@ -13,7 +13,15 @@ const Page1 = () => {
         />
       </Helmet>
       <div>FIRST PAGE</div>
-      
+      <iframe
+        width='560'
+        height='315'
+        src='https://www.youtube.com/embed/-QjKbULq7CE'
+        title='YouTube video player'
+        frameborder='0'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+        allowfullscreen
+      ></iframe>
     </>
   );
 };
